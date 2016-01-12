7d3afdb0ea52e586bf7eb2e26921f34f14dfa438
Lady Blue <lady.blue2015@yahoo.com>
2016-01-11 16:55:44 -0800

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var ChirpInput = require('./ChirpInput');
+var actions = require('../actions');
+var ChirpList = require('./ChirpList');
+var ChirpStore = require('../stores/chirps');
+
+var Home = React.createClass({
+	getInitialState: function () {
+		return {
+			chirps: ChirpStore.timeline()
+		};
+	},
+	mixins: [ChirpStore.mixin],
+	onChange: function () {
+		this.setState(this.getInitialState());
+	},
+	render: function () {
+		return <div>
+			<ChirpInput onSave={this.saveChirp} />
+			<ChirpList chirps={this.state.chirps} />
+		</div>;
+	},
+	saveChirp: function (text) {
+		actions.chirp(text);
+	}
+});
+
+module.exports = Home; 
